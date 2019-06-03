db43b020e40767fcc1d12738528b2ba9aa558f6e
Pedro Henrique Santos Pereira <pedro.santos.pereira@accenture.com>
2019-06-03 15:33:20 -0300

    Projeto xoFebre android completo

---
+++ app/src/main/java/com/jpd/xofebre/Diagnostico.java
@@ -0,0 +1,13 @@
+package com.jpd.xofebre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Diagnostico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnostico);
+    }
+}
